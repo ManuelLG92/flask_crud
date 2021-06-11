ad7ceb3b123197399a69dcf8a1b3cc07146a7358
ManuelLG92 <lavacars.ecologic@gmail.com>
2021-06-11 06:48:49 +0200

    credential bbdd

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql://manuel:manuel@localhost:3306/flask'
+SQLALCHEMY_DATABASE_URI = 'mysql://user:pass@localhost:3306/flask'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
